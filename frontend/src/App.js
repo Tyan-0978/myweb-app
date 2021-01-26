bde6b71bd50df5c3b104111f9801bc757829619b
Tyan-0978 <960007nathan@gmail.com>
2021-01-27 02:00:30 +0800

    testing a simple Navbar

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,8 +1,21 @@
-//import React from 'react';
+import Navbar from 'react-bootstrap/Navbar';
+import Nav from 'react-bootstrap/Nav';
 
 function App() {
   return (
-    <div>Tyan's app</div>
+    <>
+      <Navbar bg='dark' variant='dark'>
+        <Navbar.Brand href='/'>田祐行</Navbar.Brand>
+	<Navbar.Toggle aria0controls='main-navbar' />
+	<Navbar.Collapse id='main-navbar'>
+	  <Nav className="mr-auto">
+	    <Nav.Link href='#'>Home</Nav.Link>
+	    <Nav.Link href='#'>Blog</Nav.Link>
+	    <Nav.Link href='#'>Comments</Nav.Link>
+	  </Nav>
+	</Navbar.Collapse>
+      </Navbar>
+    </>
   );
 }
 
