525e3b33c5f0e4feb032a1b49563b9bbe285ff99
Tyan-0978 <960007nathan@gmail.com>
2021-01-28 16:37:44 +0800

    finish a simple home page

--- frontend/src/home/Home.js
+++ frontend/src/home/Home.js
@@ -1,8 +1,26 @@
 import React from 'react';
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+import Image from 'react-bootstrap/Image'
+import './Home.css';
 
 function Home() {
   return (
-    <div>my home</div>
+    <Container className="home-container">
+      <Row>
+        <Col sm={4}>
+          <Image
+	    src="https://i.imgur.com/TwElKk7.jpg?2"
+	    alt="picture" rounded fluid
+	  />
+        </Col>
+        <Col sm={8} className="description-col">
+	  <h1>田祐行</h1>
+	  <p>寫了一個網站但不知道內文要打什麼的人</p>
+	</Col>
+      </Row>
+    </Container>
   );
 }
 
