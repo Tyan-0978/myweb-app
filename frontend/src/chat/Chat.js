6c4cbb8b6fa3cf80dee857c12be9bf27796e009d
Tyan-0978 <960007nathan@gmail.com>
2021-02-11 01:35:18 +0800

    working on chat input

--- frontend/src/chat/Chat.js
+++ frontend/src/chat/Chat.js
@@ -3,6 +3,9 @@ import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
 import ListGroup from 'react-bootstrap/ListGroup';
+import Form from 'react-bootstrap/Form';
+import InputGroup from 'react-bootstrap/InputGroup';
+import FormControl from 'react-bootstrap/FormControl';
 
 import './chat.css';
 import useSize from './useSize.js';
@@ -44,12 +47,25 @@ function Chat(props) {
 	    </ListGroup.Item>
 	  </ListGroup>
 	</Col>
+
 	<Col sm={9} style={{ height: bodyHeight }}>
-	  <div className="chat-messages" style={{ height: bodyHeight - 30 }}>
-	    messages
-	  </div>
+	  <ul className="chat-messages" style={{ height: bodyHeight - 50 }}>
+	    <li>one</li>
+	    <li>two</li>
+	  </ul>
 	  <div className="chat-sender">
-	    chat sender
+	    <Form>
+	      <Form.Row>
+	        <Col sm={4}>
+		  <InputGroup>
+		    <InputGroup.Prepend>
+		      <InputGroup.Text>暱稱</InputGroup.Text>
+		    </InputGroup.Prepend>
+		    <FormControl />
+		  </InputGroup>
+		</Col>
+	      </Form.Row>
+	    </Form>
 	  </div>
 	</Col>
       </Row>
