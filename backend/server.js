e166f450f5cbce8fa381ca59f0f6b7e19c72d8e3
Tyan-0978 <960007nathan@gmail.com>
2021-07-01 20:42:01 +0800

    add log for connection error

--- backend/server.js
+++ backend/server.js
@@ -14,14 +14,24 @@ const io = new Server(httpServer, {
 const __dirname = path.resolve();
 
 io.on("connection", (socket) => {
-  // console.log("server connected");
+  console.log("server connected");
 });
 
+// connection error
+io.engine.on("connection_error", (err) => {
+  console.log(err.req);	     // the request object
+  console.log(err.code);     // the error code, for example 1
+  console.log(err.message);  // the error message, for example "Session ID unknown"
+  console.log(err.context);  // some additional error context
+});
+
+// serve frontend html file
 app.use(express.static(path.join(__dirname, '../frontend/build')));
 app.get('/*', (req, res) => {
   res.sendFile(path.join(__dirname, '../frontend/build', 'index.html'));
 });
 
-app.listen(9000);
-
-httpServer.listen(4000);
+app.listen(9000, () => {
+  console.log("Express server running.");
+});
+//httpServer.listen(4000);
